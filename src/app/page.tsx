cc4fcc9031c0c002ce79ca0482d3b17981515cb8
ManojGollamandala <manojgollamandala1@gmail.com>
2025-09-17 01:15:12 +0530

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,20 @@ import Navbar from "./components/Navbar";
 import Category from "./components/Category";
 import AddWidgetForm from "./components/AddWidgetForm";
 
+
+interface Category {
+  id: string; 
+}
+
 export default function DashboardPage() {
-  const { categories } = useDashboard();
+  
+  const { categories }: { categories: Category[] } = useDashboard();
 
   return (
     <div>
       <Navbar />
       <div className="space-y-6 p-4">
+        
         {categories.map((cat) => (
           <div key={cat.id}>
             <Category category={cat} />
